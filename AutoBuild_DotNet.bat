2f1d72f35915bd06e84c5671437c6fa4b24edc4c
Andrew Piper <threax@users.noreply.github.com>
2014-12-24 16:40:28 +0000

    Added auto build for all .net libraries.

---
+++ AutoBuild_DotNet.bat
@@ -0,0 +1,12 @@
+::Configuration Settings
+set SolutionName=build\vs2012\core\Lucene.Net.Core.sln
+
+::Less likely to need to change these.
+set ThisFolder=%~dp0
+set RootDependencyFolder=%ThisFolder%..\
+set BuildCommand="C:\Program Files (x86)\MSBuild\12.0\Bin\msbuild.exe"
+
+set SolutionPath=%ThisFolder%%SolutionName%
+
+%BuildCommand% "%SolutionPath%" /property:Configuration=Release;Platform="Any CPU" /target:Clean
+%BuildCommand% "%SolutionPath%" /property:Configuration=Release;Platform="Any CPU"
